7e41ec27001cb81a903cd8a626d75bcbd906b90b
georgedmendoza <georgedmendoza13@gmail.com>
2021-08-17 13:34:33 -0500

    use prototype

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -1,10 +1,8 @@
+const Player = require('../lib/Player.js');
 const Potion = require('../lib/Potion');
 jest.mock('../lib/Potion.js');
 
-console.log(new Potion());
-
-const Player = require('../lib/Player.js');
-const { expect } = require('@jest/globals');
+// console.log(new Potion());
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -18,3 +16,23 @@ test('creates a player object', () => {
         expect.arrayContaining([expect.any(Object)])
     );
 });
+
+test('gets players stats as an object', () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
+  });
